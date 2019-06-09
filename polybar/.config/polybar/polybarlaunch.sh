010fdb0afc2d2e673a57481422b1d1c172e42bc7
Alex <awh4kc@virginia.edu>
2019-06-09 18:10:46 -0400

    yeet

--- polybar/.config/polybar/polybarlaunch.sh
+++ polybar/.config/polybar/polybarlaunch.sh
@@ -8,10 +8,10 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar top -r -c ~/dotfiles/polybar/.config/polybar/polybar.conf &
-    MONITOR=$m polybar bottom -r -c ~/dotfiles/polybar/.config/polybar/polybar.conf &
+    MONITOR=$m polybar top -r -c ~/.dotfiles/polybar/.config/polybar/polybar.conf &
+    MONITOR=$m polybar bottom -r -c ~/.dotfiles/polybar/.config/polybar/polybar.conf &
   done
 else
-  polybar top -r -c ~/dotfiles/polybar/.config/polybar/polybar.conf &
+  polybar top -r -c ~/.dotfiles/polybar/.config/polybar/polybar.conf &
 fi
 
