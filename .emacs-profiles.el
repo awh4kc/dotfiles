2fe3c8f536525550d962208789ac74d131b41c81
Alex <awh4kc@virginia.edu>
2019-11-04 22:10:38 -0500

    yeet

---
+++ .emacs-profiles.el
@@ -0,0 +1,6 @@
+(("default" . ((user-emacs-directory . "~/Documents/misc/emacs/doom-emacs")))
+ ("spacemacs" . ((user-emacs-directory . "~/Documents/misc/emacs/spacemacs")))
+ ("prelude" . ((user-emacs-directory . "~/Documents/misc/emacs/prelude")))
+ ("centaur" . ((user-emacs-directory . "~/Documents/misc/emacs/centaur")))
+ ("scimax" . ((user-emacs-directory . "~/Documents/misc/emacs/scimax")))
+ ("vanilla" . ((user-emacs-directory . "~/.emacs.d"))))
